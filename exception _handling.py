e3bb1d461d45f3a5bd7f85cca6f1b196c0b71cc1
Cheyenne Tyrell Beharie <cheyennetyrell@gmail.com>
2025-07-01 12:35:45 +0200

    Add files via upload

--- exception _handling.py
+++ exception _handling.py
@@ -1,7 +1,12 @@
 
 try:
     print(x)
-except:
-    print("Something went wrong!")
-finally:
-    print("the 'try except' is finished")
+except NameError:
+    print("variable x is not defined")
+else:
+    print("everything went wrong")
+    
+x = -1
+
+if x < 0:
+    raise Exception("Sorry, no numbers below zero")
